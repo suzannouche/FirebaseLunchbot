f7011edab0d0e1de94727c824e720ad315188b64
Kirsten Schubert <kirsten.schubert@ptml.com>
2019-11-28 16:00:26 +1300

    adding to it

--- public/orderhistory.js
+++ public/orderhistory.js
@@ -15,33 +15,44 @@ function loadOrderHistory() {
 
   var lastTenOrders = firestore.collection("orders").limit(10).get();
   lastTenOrders.then(querySnapshot => {
-  //firestore.collection("orders").get().then(querySnapshot => {
       querySnapshot.forEach((order) => {
         
         console.log('log', order.data());
 
           var allRestaurantsPerOrder = []
-          order.ref.collection("orderrestaurants").get().then(documents => {
+          order.ref.collection("orderrestaurants").get()
+          .then(documents => {
               documents.docs.map(restaurant => {
               //console.log('Found subcollection with id:', restaurant.data().name);
               allRestaurantsPerOrder.push(restaurant.data().name);
             });
-          });          
+          })
+          .catch(err => {
+            console.log('Error getting subcollections', err)
+          });
+
           console.log('all restaurants:', allRestaurantsPerOrder);
           addRow(tableOrders, allRestaurantsPerOrder, order.data().date, order.data().price, order.data().comment, order.data().id);
 
-          if (order.data().id == 2) {
-            var orderObj = firestore.collection("orders").where('id', '==', order.data().id).limit(1).get();
-            console.log("order: ", orderObj);
-            orderObj.update({
-              "comment": "updated comment"
-              })
-              .then(function() {
-                console.log("Order comment successfully updated!");
-              });
-          }
+      //  if (order.data().id == 2)   {
+      //       firestore.collection("orders").where('id', '==', order.data().id).get()
+      //       .then(snapshot => {
+      //           snapshot.forEach((order) => {
+      //             console.log('order', order.ref);      
+      //             order.ref.update({
+      //               comment: "this is"
+      //               })
+      //               .then(function() {
+      //                 console.log("Order comment successfully updated!");
+      //               })
+      //           });     
+      //       });
+      //   }
       })
-    });  
+    })
+    .catch(err => {
+      console.log('Error getting orders', err)
+    });;  
    };
 
 function addCell(tr, val) {
@@ -56,6 +67,23 @@ function addCellWithElement(tr, element) {
   tr.appendChild(td)
 }
 
+function saveComment(orderId) {
+  const inputValue  = document.querySelector("#order_" + orderId).value;
+  firestore.collection("orders").where("id", "==", orderId).get()
+  .then(function(snapshot) {
+      snapshot.forEach(function(order) {
+        console.log('order', order.ref);      
+        order.update({
+          comment: inputValue
+          })
+          .then(function() {
+            console.log("Order comment successfully updated!");
+          })
+      });     
+  })
+  .catch(function(error){console.log(error);});
+}
+
 function addRow(tbl, restaurant, date, price, comment, orderId) {
   var tr = document.createElement('tr');
   addCell(tr, restaurant);
@@ -63,24 +91,24 @@ function addRow(tbl, restaurant, date, price, comment, orderId) {
   addCell(tr, price);
 
   var cmt = document.createElement("input");
-  cmt.type= "text"
-  cmt.value = comment
+  cmt.type = "text";
+  cmt.id = "order_" + orderId;
+  cmt.value = comment;
   addCellWithElement(tr, cmt);
 
   var btn = document.createElement('button');
+  btn.id = orderId;
   btn.innerHTML = "Save comment";
-  btn.onclick = function saveComment(orderId, comment) {
-    var orderObj = firestore.collection("orders").where('id', '==', orderId);
-    orderObj.update({
-      "comment": comment
-      })
-      .then(function() {
-        console.log("Order comment successfully updated!");
-      });
-  }
-  addCellWithElement(tr, btn);
+  // firestore.collection('orders').doc(order.id).update({comment: orderId})
+      // .then(function() {
+      //   console.log("Order comment successfully updated!");
+      // });
 
-  //use orderId for functioncall
+  btn.onclick = function(){saveComment(orderId)};
+  
+  addCellWithElement(tr, btn);
 
   tbl.appendChild(tr)
 }
+
+
